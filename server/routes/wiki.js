3795a37770fa068801b22f320b1709d50d022952
Leonardo Calasans <calasans.leonardo@gmail.com>
2025-08-19 19:57:09 -0300

    Update wiki.js

--- server/routes/wiki.js
+++ server/routes/wiki.js
@@ -146,7 +146,7 @@ router.get('/opus', async function(req, res) {
         srsearch: wikiSearchParam
     }
     
-    request({ url: searchUrl, qs: searchParams, json: true, headers: {'User-Agent': 'MusicPearls (https://www.musicpearls.org/about)'}, (err, response, searchData) => {
+    request({ url: searchUrl, qs: searchParams, json: true, headers: {'User-Agent': 'MusicPearls (https://www.musicpearls.org/about)'}}, (err, response, searchData) => {
         if (err) {
             console.error('Error searching Wikipedia: ', err);
             return res.status(500).send('Error searching Wikipedia');
@@ -183,7 +183,7 @@ router.get('/opus', async function(req, res) {
                 titles: wikiTitle
             }
 
-            request( {url: searchUrl, qs: pageQueryParam, json: true, headers: {'User-Agent': 'MusicPearls (https://www.musicpearls.org/about)'}, (err, response, opusData) => {
+            request( {url: searchUrl, qs: pageQueryParam, json: true, headers: {'User-Agent': 'MusicPearls (https://www.musicpearls.org/about)'}}, (err, response, opusData) => {
                 if (err) {
                     console.error('Error finding the work page: ', err);
                     return res.status(404).send('Work page not found in Wikipedia')
